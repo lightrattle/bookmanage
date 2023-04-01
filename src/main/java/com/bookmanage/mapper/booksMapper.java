c78d733630721b1873e490036c9ec901d60128ce
lightrattle <3555878252@qq.com>
2023-04-01 23:03:47 +0800

    book表中数据操作之增、查、借出时对借出次数和剩余数量的修改。

--- src/main/java/com/bookmanage/mapper/booksMapper.java
+++ src/main/java/com/bookmanage/mapper/booksMapper.java
@@ -2,6 +2,18 @@ package com.bookmanage.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+import java.util.Map;
+
 @Mapper
 public interface booksMapper {
+    public List<Map<String, Object>> getAllBooks();
+
+    public List<Map<String, Object>> getBooksByTypes(String booktype);
+
+    public List<Map<String, Object>> getBooksOfLendnum();
+
+    public boolean insertOneBook(String bookname, String booktype, String image, int allnum, String intro);
+
+    public boolean updateLendnum(int bookid);
 }
