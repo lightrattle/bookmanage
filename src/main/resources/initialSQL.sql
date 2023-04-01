b72bd79e3623407d486ffbb84a678e5af3e4b509
lightrattle <3555878252@qq.com>
2023-04-01 19:30:31 +0800

    项目初始化

---
+++ src/main/resources/initialSQL.sql
@@ -0,0 +1,12 @@
+/*
+ Navicat Premium Data Transfer
+ Source Server : 本地
+ Source Server Type : MySQL
+ Date: 2/25/2023 15:34:34
+*/
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+
+
+SET FOREIGN_KEY_CHECKS = 1;
