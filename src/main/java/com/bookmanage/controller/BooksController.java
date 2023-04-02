4e00b07fc1d9d001b4b6b15603b1eed5aa9c7692
lightrattle <3555878252@qq.com>
2023-04-02 15:09:14 +0800

    Controller层文件创建

---
+++ src/main/java/com/bookmanage/controller/BooksController.java
@@ -0,0 +1,12 @@
+package com.bookmanage.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/books", method={RequestMethod.GET, RequestMethod.POST})
+@EnableAutoConfiguration
+public class BooksController {
+}
